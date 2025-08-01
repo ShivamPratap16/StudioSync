60f1b5c069e817246a99ccb9e55023c1fd9244bd
ShivamPratap16 <138383845+ShivamPratap16@users.noreply.github.com>
2025-08-02 03:26:57 +0530

    dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "./Components/ThemeProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -8,18 +9,20 @@ import EditorDashboard from "./pages/Editor/dashboard";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/youtuber">
-          <Route path="dashboard" element={<YouTuberDashboard />} />
-          <Route path="upload" element={<UploadProject />} />
-        </Route>
-        <Route path="/editor/dashboard" element={<EditorDashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/youtuber">
+            <Route path="dashboard" element={<YouTuberDashboard />} />
+            <Route path="upload" element={<UploadProject />} />
+          </Route>
+          <Route path="/editor/dashboard" element={<EditorDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
